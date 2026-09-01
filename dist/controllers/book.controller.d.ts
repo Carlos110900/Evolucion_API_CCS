975c7a04c7144af89e10c486efb49daa030c2fbd
Carlos110900 <carlos.cevallos@iteso.mx>
2026-09-01 16:44:45 -0600

    Delete dist directory

--- dist/controllers/book.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Request, Response } from "express";
-export declare function getAllBooks(req: Request, res: Response): void;
-export declare function getBook(req: Request, res: Response): void;
-export declare function addBook(req: Request, res: Response): void;
-export declare function removeBook(req: Request, res: Response): void;
-export declare function patchBook(req: Request, res: Response): void;
-//# sourceMappingURL=book.controller.d.ts.map
--- dist/services/book.service.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Book } from "../types/book.js";
-export declare function getBooks(author?: string): Book[];
-export declare function getBookById(id: number): Book;
-export declare function createBook(bookData: Omit<Book, "id">): Book;
-export declare function deleteBook(id: number): void;
-export declare function updateBook(id: number, changes: Partial<Omit<Book, "id">>): Book;
-//# sourceMappingURL=book.service.d.ts.map
